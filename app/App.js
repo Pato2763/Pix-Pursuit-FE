871423281387f3bd6afcc096acc90df9d33cbfff
Pato2763 <daviespatrick2763@gmail.com>
2024-11-19 16:26:42 +0000

    addded header to all the pages

--- app/App.js
+++ app/App.js
@@ -5,13 +5,14 @@ import { View, Text } from "react-native";
 import HomeScreen from "./screens/HomeScreen";
 import Leaderboard from "./screens/Leaderboard";
 import { SafeAreaView } from "react-native-safe-area-context";
+import Header from "./components/Header";
 
-// Create Bottom Tab Navigator
 const Tab = createBottomTabNavigator();
 
 function App() {
   return (
     <NavigationContainer>
+      <Header />
       <Tab.Navigator
         initialRouteName="Home"
         screenOptions={{
