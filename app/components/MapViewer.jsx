296c91867e01ea524a43655c7727b4ca963b342c
Phillywisher <willfisher467@gmail.com>
2024-11-20 16:39:35 +0000

    mapView

--- app/components/MapViewer.jsx
+++ app/components/MapViewer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import MapView, { Marker } from "react-native-maps";
 import { StyleSheet, View, Text } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-
+import Colours from "../utils/Colours";
 export const MapViewer = ({ trackedLocation }) => {
   if (
     !trackedLocation ||
@@ -14,10 +14,7 @@ export const MapViewer = ({ trackedLocation }) => {
 
   return (
     <SafeAreaView style={styles.safeAreaContainer}>
-      <View style={styles.infoContainer}>
-        <Text>Latitude: {trackedLocation.latitude}</Text>
-        <Text>Longitude: {trackedLocation.longitude}</Text>
-      </View>
+      <View style={styles.infoContainer}></View>
       <View style={styles.container}>
         <MapView
           style={styles.map}
@@ -27,8 +24,7 @@ export const MapViewer = ({ trackedLocation }) => {
             latitudeDelta: 0.0922, //these delta thing fellas are for zooming purpopes
             longitudeDelta: 0.0421,
           }}
-          showsUserLocation={true}
-          // this is the blue dot, the Marker shows a red pin drop which we dont want
+          showsUserLocation={true} // this is the blue dot, the Marker shows a red pin drop which we dont want
         >
           {/* <Marker
             coordinate={{
@@ -45,16 +41,34 @@ export const MapViewer = ({ trackedLocation }) => {
 const styles = StyleSheet.create({
   safeAreaContainer: {
     flex: 1,
+    backgroundColor: "white",
+    borderColor: "white",
   },
   infoContainer: {
     padding: 10,
     margin: 10,
+    backgroundColor: "white",
+    borderColor: "white",
   },
   container: {
     flex: 1,
+    backgroundColor: "white",
   },
+
   map: {
-    width: "100%",
+    width: "90",
     height: "100%",
+    flex: 1,
+    gap: 20,
+    width: "80%",
+    borderRadius: 10,
+    padding: 5,
+    borderColor: "#D9D9D9",
+    borderWidth: 2,
+    alignItems: "center",
+    justifyContent: "center",
+    textAlign: "center",
+    backgroundColor: Colours.AQUA_BLUE,
+    marginHorizontal: "auto",
   },
 });
