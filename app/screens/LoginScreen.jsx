618ae553df0cd9662472f0673804e9fb5318a729
Ryan Fox <ryanfox1@hotmail.co.uk>
2024-11-20 14:25:55 +0000

    login functionality

--- app/screens/LoginScreen.jsx
+++ app/screens/LoginScreen.jsx
@@ -1,78 +1,95 @@
-import { View, Text, Button, TextInput } from "react-native";
-import React, { useState, useRef } from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  Keyboard,
+} from "react-native";
+import React, { useState, useRef, useEffect } from "react";
 import { Styles } from "../utils/styles/login";
-import { TouchableWithoutFeedback } from "react-native";
-import { Keyboard } from "react-native";
-import { TouchableOpacity } from "react-native";
 import { useNavigation } from "@react-navigation/native";
+import { loginUser } from "../api";
+import { TouchableWithoutFeedback, ActivityIndicator } from "react-native";
 
 const LoginScreen = () => {
   const [loginInfo, setLoginInfo] = useState({
     username: "",
     password: "",
   });
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
   const refPasswordInput = useRef(null);
   const navigation = useNavigation();
 
-  const focusOnPassword = () => {
-    if (refPasswordInput && refPasswordInput.current) {
-      refPasswordInput.current.focus();
-    }
+  const handleInputChange = (key, value) => {
+    setLoginInfo((prevState) => ({ ...prevState, [key]: value }));
   };
 
-  const hideKeyboard = () => {
-    Keyboard.dismiss();
+  useEffect(() => {
+    console.log("Updated loginInfo:", loginInfo.username);
+  }, [loginInfo]);
+
+  const focusOnPassword = () => {
+    refPasswordInput?.current?.focus();
+  };
+
+  const submitLogin = () => {
+    setLoading(true);
+    loginUser(loginInfo)
+      .then(() => {
+        setError(false);
+        setLoading(false);
+        navigation.navigate("Home");
+      })
+      .catch((err) => {
+        console.log(err);
+        setError(true);
+        setLoading(false);
+      });
   };
 
   return (
-    <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
       <View style={Styles.container}>
         <Text style={Styles.welcome}>Welcome to Pix Pursuit!</Text>
         <View style={Styles.signUpContainer}>
           <Text>Username:</Text>
           <TextInput
             style={Styles.inputText}
+            placeholder="Enter your username"
             returnKeyType="next"
+            value={loginInfo.username}
             onSubmitEditing={focusOnPassword}
-            onChangeText={(text) =>
-              setLoginInfo({ ...loginInfo, username: text })
-            }
-          ></TextInput>
+            onChangeText={(text) => handleInputChange("username", text)}
+          />
 
           <Text>Password:</Text>
           <TextInput
             ref={refPasswordInput}
-            returnKeyType="done"
-            onSubmitEditing={hideKeyboard}
-            secureTextEntry={true}
             style={Styles.inputText}
-            onChangeText={(text) => {
-              setLoginInfo({ ...loginInfo, password: text });
-              console.log(loginInfo);
-            }}
-          ></TextInput>
+            placeholder="Enter your password"
+            secureTextEntry
+            returnKeyType="done"
+            value={loginInfo.password}
+            onSubmitEditing={Keyboard.dismiss}
+            onChangeText={(text) => handleInputChange("password", text)}
+          />
           <TouchableOpacity>
             <Text style={{ textDecorationLine: "underline" }}>
               Forgot Password?
             </Text>
           </TouchableOpacity>
-          <TouchableOpacity
-            style={Styles.loginBtn}
-            onPress={() => {
-              navigation.navigate("Home");
-            }}
-          >
-            <Text>Log In</Text>
+          <TouchableOpacity style={Styles.loginBtn} onPress={submitLogin}>
+            {loading ? <ActivityIndicator /> : <Text>Log In</Text>}
           </TouchableOpacity>
-          <Text
-            style={{
-              width: "%100",
-              marginHorizontal: "auto",
-            }}
-          >
+          {error && <Text style={Styles.errorMsg}>Error logging you in</Text>}
+          <Text style={{ textAlign: "center", marginVertical: 10 }}>
             Not a member?
           </Text>
-          <TouchableOpacity style={Styles.signupBtn}>
+          <TouchableOpacity
+            style={Styles.signupBtn}
+            onPress={() => navigation.navigate("Signup")}
+          >
             <Text>Sign up</Text>
           </TouchableOpacity>
         </View>
