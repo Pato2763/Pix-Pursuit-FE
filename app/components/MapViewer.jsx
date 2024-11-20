e9c83a098f9a4b98dc1a42fc4c5ebf337a1de415
Phillywisher <willfisher467@gmail.com>
2024-11-20 14:25:28 +0000

    map view on home screen

--- app/components/MapViewer.jsx
+++ app/components/MapViewer.jsx
@@ -1,19 +1,19 @@
 import { getLocation, getTrackedLocation } from "../utils/loaction";
-import { View } from "react-native";
 import React from "react";
 import MapView, { Marker } from "react-native-maps";
 import { StyleSheet, View } from "react-native";
 
-export const MapView = () => {
+export const MapViewer = ({ trackedLocation }) => {
   return (
     <View style={styles.container}>
       <MapView style={styles.map}>
-        <Marker coordinate={getTrackedLocation} />
+        {trackedLocation.latitude && trackedLocation.longitude && (
+          <Marker coordinate={trackedLocation} />
+        )}
       </MapView>
     </View>
   );
 };
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
