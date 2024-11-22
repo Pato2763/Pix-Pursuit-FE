218202dbc004a2cd943f5c1e1df57341172458e3
Kyle-Wearing <kylewearing@icloud.com>
2024-11-22 10:45:20 +0000

    fixed home map premissions

--- app/utils/loaction.js
+++ app/utils/loaction.js
@@ -1,36 +1,37 @@
 import * as Location from "expo-location";
 
 exports.getLocation = (setLocation) => {
+  return Location.getCurrentPositionAsync().then((location) => {
+    setLocation({
+      latitude: location.coords.latitude,
+      longitude: location.coords.longitude,
+      timestamp: location.timestamp,
+    });
+  });
+};
+
+exports.getTrackedLocation = (setTrackedLocation) => {
   return Location.requestForegroundPermissionsAsync()
     .then(({ status }) => {
       if (status !== "granted") {
         return "permission to access loaction denied";
       }
-      return Location.getCurrentPositionAsync();
     })
-    .then((location) => {
-      setLocation({
-        latitude: location.coords.latitude,
-        longitude: location.coords.longitude,
-        timestamp: location.timestamp,
-      });
+    .then(() => {
+      return Location.watchPositionAsync(
+        {
+          accuaracy: Location.Accuracy.BestForNavigation,
+          timeInterval: 1000,
+          distanceInterval: 5,
+        },
+        (newLocation) => {
+          console.log("new location set", newLocation);
+          setTrackedLocation({
+            latitude: newLocation.coords.latitude,
+            longitude: newLocation.coords.longitude,
+            timestamp: newLocation.timestamp,
+          });
+        }
+      );
     });
-};
-
-exports.getTrackedLocation = (setTrackedLocation) => {
-  return Location.watchPositionAsync(
-    {
-      accuaracy: Location.Accuracy.BestForNavigation,
-      timeInterval: 1000,
-      distanceInterval: 5,
-    },
-    (newLocation) => {
-      console.log("new location set", newLocation);
-      setTrackedLocation({
-        latitude: newLocation.coords.latitude,
-        longitude: newLocation.coords.longitude,
-        timestamp: newLocation.timestamp,
-      });
-    }
-  );
 };
